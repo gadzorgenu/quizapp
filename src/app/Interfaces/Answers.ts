3b4ab1aa21e9ea0de1d1eeb799b8a3395b4bc315
Georgina Adzorgenu <gadzorgenu@gmail.com>
2022-01-18 17:05:16 +0000

    Added Quiz structure

---
+++ src/app/Interfaces/Answers.ts
@@ -0,0 +1,9 @@
+export interface Answers{
+    answer_a:string,
+    answer_b: string,
+    answer_c: string,
+    answer_d: string,
+    answer_e: string | null,
+    answer_f: string | null,
+    
+}
